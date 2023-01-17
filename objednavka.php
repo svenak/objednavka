e39b22114ff87f15eb3945af04a773f0631029d1
svenak <svenakbartek@gmail.com>
2023-01-17 18:28:37 +0100

    stranka objecnavka,php

---
+++ objednavka.php
@@ -0,0 +1,10 @@
+<html>  
+<body>
+<H1>Zpracování formuláře</h1>
+<?php
+ echo $_GET["jmeno"], $_GET["prijmeni"], $_GET["email"], $_GET["telefon"], $_GET["platba"], $_GET["doprava"], $_GET["ulice"], $_GET["mesto"], $_GET["psc"]?>
+    
+</body>
+</html>
+
+
